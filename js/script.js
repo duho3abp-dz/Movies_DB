788434ced439f63c4ce740ecbb8a24570bb14cec
duho3abp <DanZhidkof@yandex.ru>
2020-06-21 21:35:15 +0300

    Right decision

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const ad = document.querySelectorAll('.advertising'),
+const ad = document.querySelectorAll('.promo__adv img'),
       bg = document.querySelector('.promo__bg'),
       genre = bg.querySelector('.promo__genre'),
-      promo = document.querySelectorAll('.promo__interactive-item'),
+      promoList = document.querySelector('.promo__interactive-list'),
       movieDB = {
         movies: [
             "Логан",
@@ -20,9 +20,27 @@ ad.forEach(item => {
 
 genre.textContent = 'ДРАМА';
 
-bg.style.background = 'url(img/bg.jpg) center center/cover no-repeat';
+bg.style.backgroundImage = 'url("img/bg.jpg")';
 
+promoList.innerHTML = '';
 movieDB.movies.sort();
-promo.forEach((item, i)=> {
-    item.textContent = `${i + 1}. ${movieDB.movies[i]}`;
+movieDB.movies.forEach((film, i) => {
+    promoList.innerHTML += `
+        <li class="promo__interactive-item">
+            ${i + 1}. ${film}
+            <div class="delete"></div>
+        </li>
+    `;
 });
+
+// Second option fourth task!
+// promoList.innerHTML = '';
+// movieDB.movies.sort();
+// movieDB.movies.forEach((item, i)=> {
+//     promoList.insertAdjacentHTML('beforeend', `<li class="promo__interactive-item">${i + 1}. ${movieDB.movies[i]}</li>`);
+// });
+
+// First option fourth task! Error
+// promo.forEach((item, i)=> {
+//     item.textContent = `${i + 1}. ${movieDB.movies[i]}`;
+// });
